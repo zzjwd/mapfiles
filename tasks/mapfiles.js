fcf1fed5f04170451913673a29e1568f4f9e0ca1
zzjwd <zzjwd@vip.qq.com>
2016-08-24 17:43:08 +0800

    更新了多种生成

--- tasks/mapfiles.js
+++ tasks/mapfiles.js
@@ -11,21 +11,38 @@
 module.exports = function(grunt) {
   grunt.registerMultiTask('mapfiles', 'mapfiles', function() {
     var options = this.options({
-      punctuation: 'var htmls=%;',
-      separator: ','
-    });
+          punctuation: 'var htmls=[%];',
+          separator: ','
+        }),config=grunt.config.data.pkg.viewsWay,
+        _gObject=function(f){
+          var temp={},content;
+          f.src.forEach(function(key){
+            console.log(key.replace(/\/(.+?)\./g,"$1"));
+            temp[key.replace(/.*\/(.+?)\..*/g,"$1")]=grunt.file.read(key);
+          });
+          content = options.punctuation.replace("%",JSON.stringify(temp));
+          grunt.file.write(f.dest, content);
+        },
+        _gArray=function(f){
+          var src = f.src.filter(function(filepath) {
+            if (!grunt.file.exists(filepath)) {
+              grunt.log.warn('Source file "' + filepath + '" not found.');
+              return false;
+            } else {
+              return true;
+            }
+          }).map(function(filepath) {
+            return filepath.replace(/[^\/]*\/(.*)\.[^\.]*/ig,function(m,s){
+              return '"'+s+'"';
+            });
+          });
+          src = options.punctuation.replace("%",src);
+          grunt.file.write(f.dest, src);
+        };
+
     this.files.forEach(function(f) {
-      var temp={},content;
-      f.src.forEach(function(key){
-        console.log(key.replace(/\/(.+?)\./g,"$1"));
-        temp[key.replace(/.*\/(.+?)\..*/g,"$1")]=grunt.file.read(key);
-      });
-
-      content = options.punctuation.replace("%",JSON.stringify(temp));
-      grunt.file.write(f.dest, content);
+      config?_gArray(f):_gObject(f);
       grunt.log.writeln('File "' + f.dest + '" created.');
-
     });
   });
-
 };
