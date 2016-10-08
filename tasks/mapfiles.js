5a3b5c4471258a007fb92a86f6706e3deac6e52e
zzjwd <zzjwd@vip.qq.com>
2016-10-08 13:48:01 +0800

    dd

--- tasks/mapfiles.js
+++ tasks/mapfiles.js
@@ -9,40 +9,23 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.registerMultiTask('mapfiles', 'mapfiles', function() {
-    var options = this.options({
-          punctuation: 'var htmls=[%];',
-          separator: ','
-        }),config=grunt.config.data.pkg.viewsWay,
-        _gObject=function(f){
-          var temp={},content;
-          f.src.forEach(function(key){
-            console.log(key.replace(/\/(.+?)\./g,"$1"));
-            temp[key.replace(/.*\/(.+?)\..*/g,"$1")]=grunt.file.read(key);
-          });
-          content = options.punctuation.replace("%",JSON.stringify(temp));
-          grunt.file.write(f.dest, content);
-        },
-        _gArray=function(f){
-          var src = f.src.filter(function(filepath) {
-            if (!grunt.file.exists(filepath)) {
-              grunt.log.warn('Source file "' + filepath + '" not found.');
-              return false;
-            } else {
-              return true;
-            }
-          }).map(function(filepath) {
-            return filepath.replace(/[^\/]*\/(.*)\.[^\.]*/ig,function(m,s){
-              return '"'+s+'"';
+    grunt.registerMultiTask('mapfiles', 'mapfiles', function() {
+        var options = this.options({
+            punctuation: 'var htmls=%;',
+            separator: ','
+        });
+        this.files.forEach(function(f) {
+            var temp={},content;
+            f.src.forEach(function(key){
+                console.log(key.replace(/\/(.+?)\./g,"$1"));
+                temp[key.replace(/.*\/(.+?)\..*/g,"$1")]=grunt.file.read(key);
             });
-          });
-          src = options.punctuation.replace("%",src);
-          grunt.file.write(f.dest, src);
-        };
 
-    this.files.forEach(function(f) {
-      config?_gArray(f):_gObject(f);
-      grunt.log.writeln('File "' + f.dest + '" created.');
+            content = options.punctuation.replace("%",JSON.stringify(temp));
+            grunt.file.write(f.dest, content);
+            grunt.log.writeln('File "' + f.dest + '" created.');
+
+        });
     });
-  });
+
 };
